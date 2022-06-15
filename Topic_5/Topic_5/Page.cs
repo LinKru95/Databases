a127b2c5806607f4b58495d013b9c87b4586f3d9
LinKru95 <100715343+LinKru95@users.noreply.github.com>
2022-06-15 20:55:24 +0300

    Initial commit

---
+++ Topic_5/Topic_5/Page.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_5
+{
+    public class Page
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public string Content { get; set; }
+        public Page(int number, string content)
+        {
+            Id = Guid.NewGuid();
+            Number = number;
+            Content = content;
+        }
+    }
+}
---
+++ Topic_5/Topic_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Topic_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var dbContext = new BookContext();
+
+            //INSERT
+
+            //var page = new Page(1, "1 skyrius");
+            //dbContext.Pages.Add(page);
+            //dbContext.SaveChanges();
+
+            //SELECT AND DELETE
+
+            //var pageFromDb = dbContext.Pages.FirstOrDefault(x => x.Number == 1);
+            //dbContext.Pages.Remove(pageFromDb);
+            //dbContext.SaveChanges();
+
+            //SELECT AND UPDATE
+
+            //var pageFromDb = dbContext.Pages.AsNoTracking().FirstOrDefault(x => x.Number == 1);
+            //pageFromDb.Content += ". Adding update";
+            //dbContext.SaveChanges();
+
+
+        }
+    }
+}
