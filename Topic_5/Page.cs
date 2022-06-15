28c9939b0c7690a41c311a6f231a1a98328f5782
LinKru95 <100715343+LinKru95@users.noreply.github.com>
2022-06-15 21:52:47 +0300

    Databases Topic_5 classwork

--- Topic_5/Page.cs
+++ Topic_5/Page.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ namespace Topic_5
         public Guid Id { get; set; }
         public int Number { get; set; }
         public string Content { get; set; }
+        [ForeignKey("Book")]
+        public Guid BookId { get; set; }
+        public Book Book { get; set; }
         public Page(int number, string content)
         {
             Id = Guid.NewGuid();
---
+++ Topic_5/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Topic_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var dbContext = new BookContext();
+
+            //INSERT
+
+            //var page = new Page(1, "1 skyrius");
+            //dbContext.Pages.Add(page);
+            //dbContext.SaveChanges();
+
+            //SELECT AND DELETE
+
+            //var pageFromDb = dbContext.Pages.FirstOrDefault(x => x.Number == 1);
+            //dbContext.Pages.Remove(pageFromDb);
+            //dbContext.SaveChanges();
+
+            //SELECT AND UPDATE
+
+            //var pageFromDb = dbContext.Pages.AsNoTracking().FirstOrDefault(x => x.Number == 1);
+            //pageFromDb.Content += ". Adding update";
+            //dbContext.SaveChanges();
+
+            // CREATE BOOK WITH PAGES
+
+            //var book = new Book("Harry Potter");
+            //for (int i = 1; i < 500; i++)
+            //{
+            //    book.Pages.Add(new Page(i, $"content: {i}"));
+            //}
+            //dbContext.Add(book);
+            //dbContext.SaveChanges();
+
+            // SELECT BOOK WITH PAGES
+
+            //var bookFromDb = dbContext.Books.Include(x => x.Pages).FirstOrDefault(x => x.Name == "Harry Potter");
+            //Console.WriteLine(bookFromDb.Id);
+
+            // SELECT PAGE WITH BOOK
+
+            //var pageFromBook = dbContext.Pages.Include(x=>x.Book).FirstOrDefault(x => x.Number == 12);
+            //Console.WriteLine(pageFromBook.Book.Name);
+
+            // DELETE BOOK WITH PAGES
+
+            //var bookFromDb = dbContext.Books.Include(x => x.Pages).FirstOrDefault(x => x.Name == "Harry Potter");
+            //dbContext.Books.Remove(bookFromDb);
+            //dbContext.SaveChanges();
+        }
+    }
+}
